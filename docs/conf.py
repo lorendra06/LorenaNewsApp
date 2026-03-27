ee3ba82664bf2f2b0de766644d52c7d9a060f2af
Loredana <lorendra06@gmail.com>
2026-03-27 16:33:00 -0500

    docs: generate Sphinx HTML documentation

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+
+import os
+import sys
+import django
+
+sys.path.insert(0, os.path.abspath('..'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'LorenaNewsApp.settings'
+django.setup()
+
+# -- Project information -----------------------------------------------------
+
+project = 'LorenaNewsApp'
+copyright = '2026, Loredana Draghin'
+author = 'Loredana Draghin'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = ['sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,16 @@
+Welcome to LorenaNewsApp's documentation!
+==========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. automodule:: LorenasGossip.models
+   :members:
+
+.. automodule:: LorenasGossip.views
+   :members:
+
+.. automodule:: LorenasGossip.forms
+   :members:
+
